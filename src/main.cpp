a8b5115a0ab7d8e23df947ab9448feb64c825e22
Alex <alex.grabski555@gmail.com>
2023-11-24 21:05:55 +0100

    key press listening

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,43 @@ int main()
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(1280, 820, 0, &window, &renderer);
+	SDL_Event event;
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_CreateWindowAndRenderer(1280, 720, 0, &window, &renderer);
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderDrawPoint(renderer, 1280/2, 820/2);
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(10000);
+	bool running = true;
+	while(running)
+	{
+		// eventListener?
+		while(SDL_PollEvent(&event))
+		{
+			// If window X clicked, close the program
+			if(event.type == SDL_QUIT)
+				running = false;
+
+			// Listen for key press (right arrow)
+			if(event.type == SDL_KEYDOWN)
+			{
+				switch(event.key.keysym.sym)
+				{
+					case SDLK_RIGHT:
+						std::cout << "key right was pressed" << std::endl;
+				}
+			}
+
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderDrawPoint(renderer, 1280/2, 720/2);
+
+		SDL_RenderPresent(renderer);
+		SDL_Delay(10);
+	}
 
 	return 0;
 }
